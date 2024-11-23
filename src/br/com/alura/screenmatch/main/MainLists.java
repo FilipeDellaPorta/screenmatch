16aafa50ed2b83c44f567002db6c1edb294fa55f
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2024-11-23 14:08:36 -0300

    lidando com tipos de objetos diferentes em uma lista

---
+++ src/br/com/alura/screenmatch/main/MainLists.java
@@ -0,0 +1,30 @@
+package br.com.alura.screenmatch.main;
+
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
+
+import java.util.ArrayList;
+
+public class MainLists {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme("O poderoso chefão", 1972);
+        meuFilme.avalia(9);
+        Filme meuFilme2 = new Filme("Avatar", 2009);
+        meuFilme2.avalia(6);
+        var filmeDoPaulo = new Filme("Dogville", 2003);
+        filmeDoPaulo.avalia(10);
+        Serie lost = new Serie("Lost", 2004);
+
+        ArrayList<Titulo> listaDeTitulos = new ArrayList<>();
+        listaDeTitulos.add(filmeDoPaulo);
+        listaDeTitulos.add(meuFilme);
+        listaDeTitulos.add(meuFilme2);
+        listaDeTitulos.add(lost);
+        for (Titulo titulo: listaDeTitulos) {
+            System.out.println(titulo.getNome());
+            Filme filme = (Filme) titulo;
+            System.out.println("Classificação: " + filme.getClassificacao());
+        }
+    }
+}
